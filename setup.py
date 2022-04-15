d5e082655896683a3cb7f1be6dd48db0f9b19490
ErissonCalado <74382139+ErissonCalado@users.noreply.github.com>
2022-04-15 17:17:18 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ with open("requirements.txt") as f:
 setup(
     name="package_name",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="Erisson Calado",
+    author_email="erisson2@gmail.com",
+    description="desafio",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/ErissonCalado/dio_projeto1"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
